27d72d112bae1814e9612d7d311ecc018cba89a2
sky2d <143280010+sky2d@users.noreply.github.com>
2026-09-03 11:28:54 +0530

    fix(storefront): remove unused ErrorPage import causing vercel build failure

--- apps/storefront/src/app/store/[name]/products/page.tsx
+++ apps/storefront/src/app/store/[name]/products/page.tsx
@@ -5,7 +5,6 @@ import { SimpleProduct } from "components/src/interfaces";
 import { getBestSellingProduct, getNewArrivalProduct, searchProducts, searchProductsByTag } from "components/src/services/api";
 import dynamic from "next/dynamic";
 
-const ErrorPage = dynamic(() => import("components/src/module/ErrorPage").then(mod => mod.ErrorPage), { ssr: false });
 
 const AllProduct = dynamic(() => import("./AllProducts"), {
   ssr: true,
